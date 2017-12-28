bbfb5459cec328fa4056da9aebb5eede0ac7f91e
vimniky <vimniky@gmail.com>
2017-12-28 11:58:25 +0800

    Update Asean ui catalog

--- asean-ui/containers/0.0.1/entry.sh
+++ asean-ui/containers/0.0.1/entry.sh
@@ -2,18 +2,10 @@
 
 set -e
 
-for i in $(curl -s --unix /var/run/docker.sock http://localhost/info | jq -r .DockerRootDir) /var/lib/docker /run /var/run; do
-    for m in $(tac /proc/mounts | awk '{print $2}' | grep ^${i}/); do
-        if [ "$m" != "/run/log/journal" ]&&[ "$m" != "/run/docker.sock" ]; then
-            umount $m || true
-        fi
-    done
-done
-
 confd --backend rancher --prefix /2016-07-29 &
 
 # Wait for confd to create initial config files
-while [ ! -f /fluentd/etc/fluent-generated.conf ]; do
+while [ ! -f /etc/nginx/nginx.conf ]; do
   sleep 1
 done
 
